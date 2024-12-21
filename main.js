b9aa99f2fe5a91ea9c9fd7ed07a0b58d0b9d2b00
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2024-12-21 15:11:28 +0700

    Finish 9. Use computed property

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
-const { createApp, ref } = Vue;
+const { createApp, ref, computed } = Vue;
 
 createApp({
   setup() {
     const product = ref("Boots");
+    const brand = ref("SE 331");
     const description = ref("These are the best boots you will ever find!");
-    const image = ref("./assets/images/socks_green.jpg");
     const link = ref("http://www.camt.cmu.ac.th");
-    const inStock = ref(true);
+    // 8.3
+    const image = computed(() => variants.value[selectedVariant.value].image);
+    const inStock = computed(() => variants.value[selectedVariant.value].quantity);
+    //
     const inventory = ref(100);
     const onSale = ref(true); // 4.9
 
     const details = ref(["50% cotton", "30% wool", "20% polyester"]);
     const variants = ref([
-      { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
-      { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
+      { id: 2234, color: "green", image: "./assets/images/socks_green.jpg", quantity: 50 },
+      { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg", quantity: 0 },
     ]);
+    const selectedVariant = ref(0);
+
     const sizes = ref(["S", "M", "L"]);
 
     // 6
@@ -30,9 +35,16 @@ createApp({
         inStock.value = !inStock.value;
         inventory.value = inStock.value ? 100 : 0;
     }
+
+    // 8
+    const title = computed(() => `${brand.value} ${product.value} ${onSale.value ? "is on sale" : ""}`);
+
+    function updateVariant(index) {
+      selectedVariant.value = index;
+    }
     
     return {
-      product,
+      title,
       description,
       image,
       link,
@@ -46,6 +58,7 @@ createApp({
       addToCart,
       updateImage,
       toggleStock, // 6.7
+      updateVariant, // 8
     };
   },
 }).mount("#app");
